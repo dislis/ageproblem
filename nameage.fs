c52f3089caa8fd3f8517c18a620d7d8ad11bc6d0
dislis <dislis@hotmail.com>
2015-10-11 21:48:43 +0300

    Create nameage.fs

---
+++ nameage.fs
@@ -0,0 +1,25 @@
+open System
+
+let checkage() =
+    let canparse, keyin = Int32.TryParse(Console.ReadLine())
+    if canparse then keyin
+    else 0
+
+let ageprint (name:string) (age:int) :string=
+    if age >= 20 then
+       name + " " + age.ToString() + " is no longer a teenager"
+    elif age >13 && age<20 then
+       name + " " + age.ToString() + " is a teenage" 
+    else
+        name + " " + age.ToString() + " is a child"
+
+[<EntryPoint>]
+let main argv = 
+    Console.WriteLine("Give a name")
+    let inputname =Console.ReadLine()
+    Console.WriteLine("Give an age")
+    let inputage = checkage()
+    let output =ageprint inputname   inputage
+    Console.WriteLine(output)
+    Console.ReadKey()
+    0 // return an integer exit code
